44477c6a341fee87596b0f968e614907431bec53
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-01-31 16:52:24 +0100

    done

--- src/utils.c
+++ src/utils.c
@@ -12,36 +12,38 @@
 
 #include "pipex.h"
 
-void	error_exit(const char *msg, int code)
+static void cmd_not_found(char *cmd, char **args, int perm_denied)
 {
-	if (msg)
+	ft_putstr_fd("pipex: ", STDERR_FILENO);
+	if (perm_denied)
+		perror(cmd);
+	else
 	{
-		ft_putstr_fd("pipex: ", STDERR_FILENO);
-		perror(msg);
+		ft_putstr_fd(cmd, STDERR_FILENO);
+		ft_putendl_fd(": command not found", STDERR_FILENO);
 	}
-	exit(code);
+	free_split(args);
+	if (perm_denied)
+		exit(126);
+	exit(127);
 }
 
-void	free_split(char **arr)
+static void execve_fail(char *cmd, char *path, char **args)
 {
-	size_t	i;
-
-	if (!arr)
-		return;
-	i = 0;
-	while (arr[i])
-	{
-		free(arr[i]);
-		i++;
-	}
-	free(arr);
+	ft_putstr_fd("pipex: ", STDERR_FILENO);
+	perror(cmd);
+	free(path);
+	free_split(args);
+	if (errno == EACCES)
+		exit(126);
+	exit(127);
 }
 
-void	exec_cmd(char *cmd_str, char **envp)
+void exec_cmd(char *cmd_str, char **envp)
 {
-	char	**args;
-	char	*path;
-	int		perm_denied;
+	char **args;
+	char *path;
+	int perm_denied;
 
 	args = split_cmd(cmd_str);
 	if (!args || !args[0])
@@ -52,29 +54,32 @@ void	exec_cmd(char *cmd_str, char **envp)
 	perm_denied = 0;
 	path = get_cmd_path(args[0], envp, &perm_denied);
 	if (!path)
-	{
-		if (perm_denied)
-		{
-			ft_putstr_fd("pipex: ", STDERR_FILENO);
-			perror(args[0]);
-		}
-		else
-		{
-			ft_putstr_fd("pipex: ", STDERR_FILENO);
-			ft_putstr_fd(args[0], STDERR_FILENO);
-			ft_putendl_fd(": command not found", STDERR_FILENO);
-		}
-		free_split(args);
-		if (perm_denied)
-			exit(126);
-		exit(127);
-	}
+		cmd_not_found(args[0], args, perm_denied);
 	execve(path, args, envp);
-	ft_putstr_fd("pipex: ", STDERR_FILENO);
-	perror(args[0]);
-	free(path);
-	free_split(args);
-	if (errno == EACCES)
-		exit(126);
-	exit(127);
+	execve_fail(args[0], path, args);
+}
+
+void error_exit(const char *msg, int code)
+{
+	if (msg)
+	{
+		ft_putstr_fd("pipex: ", STDERR_FILENO);
+		perror(msg);
+	}
+	exit(code);
+}
+
+void free_split(char **arr)
+{
+	size_t i;
+
+	if (!arr)
+		return;
+	i = 0;
+	while (arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
 }
